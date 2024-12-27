bf57a53293b95721b2d78c4818436ad5691ab0d8
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-12-27 10:01:16 +0100

    Update fedora-ostree.sh

--- fedora-ostree.sh
+++ fedora-ostree.sh
@@ -1,19 +1,41 @@
 # Functions for Fedora
 
-# start
-
-
 #### LIST OF FUNCTIONS
 
-#
-#
-#
-#
-#
-#
-#
-#
-
+### Terra repos
+f_terra
+### RPM-fusion
+f_rpmfusion
+### Fedora auto updates
+f_updates
+### Flatpak auto updates
+f_flatpak
+### First boot setup
+f_firstboot
+### Multimedia
+f_multimedia
+### Firefox
+f_firefox
+### Fonts
+f_fonts
+### CachyOS Kernel
+f_cachy
+### Mesa-git Mesa Freeworld
+f_mesa-freeworld
+### Mesa-git Mesa Freeworld
+f_mesa-git
+### Gaming
+f_gaming
+### Utils
+f_utils
+### GNOME
+f_gnome
+### Tailscale
+f_tailscale
+### Distrobox
+f_distrobox
+### Sublime Text
+f_sublime
 
 #===========================================================#
 
@@ -244,4 +266,3 @@ function f_sublime(){
 	#rpm -qa | sort | grep sublime-text
 }
 
-# end
