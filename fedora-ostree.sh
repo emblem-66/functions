bf7d0d9b1c95ce362eb8fe53de00cf0c4fbbf0ae
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2025-01-19 12:50:32 +0100

    Update fedora-ostree.sh

--- fedora-ostree.sh
+++ fedora-ostree.sh
@@ -187,10 +187,10 @@ function f_gaming(){
 ### Utils
 function f_utils(){
 	dnf install -y python-pip pipx
-	dnf install -y piper
-	systemctl enable ratbagd.service
-	dnf install -y input-remapper
-	systemctl enable input-remapper
+	#dnf install -y piper
+	#systemctl enable ratbagd.service
+	#dnf install -y input-remapper
+	#systemctl enable input-remapper
 }
 
 ### GNOME
